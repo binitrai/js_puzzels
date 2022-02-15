28bf7c190422d5876ed2437a5e387942eb075064
Binit Rai <binitcse@gmail.com>
2022-02-15 21:25:30 +0530

    Update bind.js

--- 2022/jspolyfills/bind.js
+++ 2022/jspolyfills/bind.js
@@ -1,1 +1,7 @@
 
+Function.prototype.newBind = function(context, ...args) {
+    const fn = this;
+    return function bound(...newArgs) {
+        return fn.apply(context, [...args, ...newArgs]);
+    }
+}
