6d78629b821e567ae51f1ff8d8bcacca6abfb5b2
Binit Rai <binitcse@gmail.com>
2022-02-21 20:27:19 +0530

    Update promiseall.js

--- 2022/jspolyfills/promiseall.js
+++ 2022/jspolyfills/promiseall.js
@@ -1,1 +1,16 @@
-
+function all(promises) {
+    return new Promise((resolve, reject) => {
+        const ret = [];
+        for (let i = 0; i < promises.length; i++) {
+          const promise = promises[i];
+          promise.then(value => {
+              ret.push(value);
+              if (ret.length === promises.length) {
+                resolve(ret);
+              }
+          }).catch(e => {
+            reject(e)
+          })
+        }
+    })
+  }
