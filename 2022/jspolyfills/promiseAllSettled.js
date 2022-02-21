2443665a7656ba406cf62d8392d09121b711f1f3
Binit Rai <binitcse@gmail.com>
2022-02-21 20:20:58 +0530

    Update promiseAllSettled.js

--- 2022/jspolyfills/promiseAllSettled.js
+++ 2022/jspolyfills/promiseAllSettled.js
@@ -1,1 +1,61 @@
 
+Promise.prototype.allSettled = function(promises) {
+    const resolvedPromises = promises.map(promise => {
+        return promise.then(value => {
+            return {
+                status : "fullFilled",
+                value : value
+            }
+        })
+        .catch(e => {
+            return {
+                status : "rejected",
+                reason : e 
+            }
+        })
+    });
+    return Promise.all(resolvedPromises);
+}
+
+/*
+Another version using async and await
+*/
+async function allR(promises) {
+    const res = []
+    for (const promise of promises) {
+        try {
+            const ret = await promise;
+            res.push({
+                status: "fullFilled",
+                value: ret
+            })
+        } catch (e) {
+            res.push({
+                status: "rejected",
+                reason: e
+            })
+        }
+    }
+    return res;
+}
+
+
+async function allRUsingFor(promises) {
+    const res = []
+    for (let i = 0; i < promises.length; i++) {
+        const promise = promises[i]
+        try {
+            const ret = await promise;
+            res.push({
+                status: "fullFilled",
+                value: ret
+            })
+        } catch (e) {
+            res.push({
+                status: "rejected",
+                reason: e
+            })
+        }
+    }
+    return res;
+}
