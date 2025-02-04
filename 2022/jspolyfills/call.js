4a93355020c2afadcfad1da2997aa8e98333a532
Binit Rai <binitcse@gmail.com>
2025-02-04 12:23:57 +0530

    Update call.js

--- 2022/jspolyfills/call.js
+++ 2022/jspolyfills/call.js
@@ -1,4 +1,10 @@
 Function.prototype.customCall = function(context, ...params) {
+    // Temporarily set the function as a method of the context
     context.fnRef = this;
-    return context.fnRef(...params)
+    // Invoke the function with the provided arguments and store in result
+    const result = context.fnRef(...params);
+    // Clean up by deleting the temporary method
+    delete context.fnRef;
+    // Return the result
+    return result;
 }
